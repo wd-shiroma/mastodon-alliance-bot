422137117c7cc7012146706d2d6007717ad4727f
wd-shiroma <wonderdream.hts@gmail.com>
2018-08-23 18:39:25 +0900

    動作改善

--- scripts/watch.sh
+++ scripts/watch.sh
@@ -6,19 +6,27 @@ cur=$(cd $(dirname $(readlink -f $0 || echo $0))/..;pwd -P)
 
 if [ -f ${cur}/process.uid ]; then
     process_id=`cat ${cur}/process.uid`
-    npx forever list | grep $process_id >/dev/null 2>&1
+    forever=`npx forever list | grep $process_id`
 
-    if [ ! $? ]; then
+    if [ "$?" = 0 ]; then
         echo "* Found process.uid, but no process."
         echo "  -> Delete process.uid"
         rm ${cur}/process.uid
+        is_exec=1
+    fi
+
+    line=`echo $forever | grep -E '^\d+:\d+:\d+' | wc -l`
+    if [ ! $line ]; then
+        echo "* Process was stopped."
+        echo "  -> kill process: $process_id"
+        npx forever stop $forever
         is_exec=1
     fi
 else
     is_exec=1
 fi
 
-if [ ${is_exec} ]; then
+if [ "${is_exec}" = 0 ]; then
     echo "The process seems to be running."
 else
     echo "* Start new process."
